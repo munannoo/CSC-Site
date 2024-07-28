4cd1f0be7c799c34d7ff4a0750e1727a23bb79d8
gauravphuyal <phuyalgaurav90@gmail.com>
2024-07-28 22:11:32 +0545

    Add initial

---
+++ src/components/data.js
@@ -0,0 +1,70 @@
+export const officerList = [
+  {
+    name: "Nij Patel",
+    title: "President",
+    linkedIn: "https://www.linkedin.com/in/nij-patel/",
+    email: "nijpatel28@gmail.com",
+    image: "../images/officers_2024/nij.png",
+    bio: "Hey everyone! My name is Nij, and I am a senior CS student here (with a minor in business!) at Pitt and this year's CSC president. I'm also a TA for a couple classees! I am super interested in the intersection of tech + business through software engineering. My freshman year I interned at Fidelity Investments and my sophomore  summer I interned at Prudential Financial. This summer, I am working at Appian. Outside of compsci, I love playing video games (I know real unique for a CS major), collecting sneakers, and watching Philly sports (forever salty about the Eagles, Phillies, and Sixers). I also enjoy tutoring at my local Kumon back home! Feel free to reach out or shoot me a message at any of the socials!"
+  },
+  {
+    name: "Julian Alamo-Rosas",
+    title: "Vice President",
+    linkedIn: "https://www.linkedin.com/in/julianalamorosas",
+    email: "jva7@pitt.edu",
+    image: "../images/officers_2024/julian.jpg",
+    bio: "Hey I’m Julian, a Senior Studying Computer science. I’m really interested in working in the industry with a focus on backend development. I have experience in the field interning at Meta, Amazon and Google in backend roles. I really enjoy playing soccer, going on a hike and have recently dabbled in DJing. My DMs are always open!"
+
+  },
+  {
+    name: "Quentin Romero Lauro",
+    title: "Business Manager",
+    linkedIn: "http://linkedin.com/in/quentinrl/",
+    email: "qtr2@pitt.edu",
+    image: "../images/officers_2024/quentin.jpeg",
+    bio: "Hey! I'm Quentin, a junior Computer Science major passionate about Human-Computer Interaction and tech policy. After freshman year, I landed an REU with CMU's Human-Computer Interaction Institute and I've continued to research there throughout my sophomore year. In the summer after sophomore year, I worked at the University of California Berkeley in the EPIC Data Lab on mixed-initiative interfaces for retrieval augmented generation pipelines. Outside of CS, you can catch me obsessing over coffee (recently got an espresso machine) or cooking family recipes. Feel free to reach out to me on any of my socials.",
+  },
+  {
+    name: "Morgan Engel",
+    title: "Director of Initiatives",
+    linkedIn: "https://www.linkedin.com/in/morgan-engel",
+    email: "mle50@pitt.edu",
+    image: "../images/officers_2024/morgan.jpg",
+    bio: "Hello! I’m Morgan, a junior in Computer Science and I am really interested in working with UX/UI and technology education. I have been researching in that area with Pitt and am super excited to watch those fields continue to grow! Over the years I have had a couple internships in Computer Science; Canary Labs, which focused on Software Engineering, and Stem Coding Labs, which was about technology and computer science education. Outside of work and classes, I really enjoy reading a nice book, baking some cookies or hanging out with family and friends. Feel free to reach out about anything!",
+  },
+  {
+    name: "Shreyash Ranjan",
+    title: "Events Coordinator",
+    linkedIn: "https://www.linkedin.com/in/shreyash-ranjan",
+    email: "shreyashran@gmail.com",
+    image: "../images/officers_2024/shreyash.jpeg",
+    bio: "Hi I’m Shreyash Ranjan! I’m a sophomore pursuing a double major in Computer science and Data science. Previously I worked at Lockheed Martin in the Software and cybersecurity space. Now I’ll be working at the Vanguard group. Outside of school and internships I love playing pickleball, cooking, hiking and traveling. Feel free to contact me at anytime!",
+  },
+  {
+    name: "Insiah Kizilbash",
+    title: "Sponsorship Coordinator",
+    linkedIn: "https://www.linkedin.com/in/insiah-kizilbash/",
+    email: "ink27@pitt.edu",
+    image: "../images/officers_2024/insiah.jpg",
+    bio: "Hey everyone! I'm Insiah, a sophomore majoring in Computer Science and minoring in Math. I'm very interested in backend development and AI (more specifically deep learning and robotics). Fun fact, I actually did robotics for all of high school which is what got me into CS. Outside of CS, I like trying out new spots to eat (i'm a big foodie!), exploring new places, and hanging out with family and friends. Don't hesitate to reach out to me on any of my socials!!",
+  },
+  {
+    name: "Danny Peelen",
+    title: "Public Relations and Communications Manager",
+    linkedIn: "http://linkedin.com/in/daniel-peelen/",
+    email: "dap276@pitt.edu",
+    image: "../images/officers_2024/danny.jpg",
+    bio: "Hello all! I'm Danny, a sophomore Computer Science major and Hungarian minor. Besides just a general interest in software development, I am also interested in Neural Networks and Natural Language Processing (NLP). I also am a TA for Discrete Structures (CS441) and a manager for the Pitt Women's Volleyball Team. My other hobbies include  volleyball, quiz bowl, and playing Minesweeper. Always willing to chat it up with anyone on my socials, never hesitate to contact me!",
+  },
+];
+
+export const initiativeLeadList = [
+  {
+    name: "Alex Lampe",
+    title: "CSC Wiki Initiative Lead",
+    linkedIn: "https://www.linkedin.com/in/alex-lampe-web-dev/",
+    email: "ajl146@pitt.edu",
+    image: "../images/officers_2023/alex.jpg",
+    bio: "Hi! I’m Alex and I'll be the CSC Wiki Initiative Lead this year. I’m a senior CS major and I’m interested in web development, data science, and machine learning. I’m also a member of the Urban Gaming Club and volunteer with Bridge Beyond. In my free time, I enjoy playing video games, reading, and biking. Feel free to reach out to me on Discord, LinkedIn, or email!",
+  }
+]
