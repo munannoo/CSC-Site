f153c51198be834115ef7438b98a2e3694b9df16
gauravphuyal <phuyalgaurav90@gmail.com>
2024-08-08 23:35:17 +0545

    Minor Updates

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Contact from "./Contact";
 import Hero from "./Hero";
 import CanvasLoader from "./Loader";
 import Navbar from "./Navbar";
-import Portfolio from "./Portfolio";
+import Portfolio from "./Events";
 import Position from "./Position";
 import SpacemanCanvas from "./Spaceman";
 import Member from "./Member";
